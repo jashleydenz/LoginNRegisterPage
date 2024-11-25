6a2f66e6ba6b343838455aa0c3709d2a532dd779
Jashley Denzel <denzelbolito24@gmail.com>
2024-11-25 12:53:07 +0800

    Add project files.

---
+++ LoginNRegisterPage/FormManageBooks.Designer.vb
@@ -0,0 +1,378 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormManageBooks
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.MinimizedWindow3 = New System.Windows.Forms.PictureBox()
+        Me.MaximizeWindow3 = New System.Windows.Forms.PictureBox()
+        Me.CloseWindow3 = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BackWindow1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.MinimizedWindow3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MaximizeWindow3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CloseWindow3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
+        CType(Me.BackWindow1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.MinimizedWindow3)
+        Me.Panel1.Controls.Add(Me.MaximizeWindow3)
+        Me.Panel1.Controls.Add(Me.CloseWindow3)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1315, 58)
+        Me.Panel1.TabIndex = 1
+        '
+        'MinimizedWindow3
+        '
+        Me.MinimizedWindow3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MinimizedWindow3.Image = Global.LoginNRegisterPage.My.Resources.Resources.MinimizedWindow
+        Me.MinimizedWindow3.Location = New System.Drawing.Point(1186, 17)
+        Me.MinimizedWindow3.Name = "MinimizedWindow3"
+        Me.MinimizedWindow3.Size = New System.Drawing.Size(28, 26)
+        Me.MinimizedWindow3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.MinimizedWindow3.TabIndex = 7
+        Me.MinimizedWindow3.TabStop = False
+        '
+        'MaximizeWindow3
+        '
+        Me.MaximizeWindow3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MaximizeWindow3.Image = Global.LoginNRegisterPage.My.Resources.Resources.MaximizeWindow
+        Me.MaximizeWindow3.Location = New System.Drawing.Point(1231, 17)
+        Me.MaximizeWindow3.Name = "MaximizeWindow3"
+        Me.MaximizeWindow3.Size = New System.Drawing.Size(28, 26)
+        Me.MaximizeWindow3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.MaximizeWindow3.TabIndex = 6
+        Me.MaximizeWindow3.TabStop = False
+        '
+        'CloseWindow3
+        '
+        Me.CloseWindow3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CloseWindow3.Image = Global.LoginNRegisterPage.My.Resources.Resources.CloseWindow
+        Me.CloseWindow3.Location = New System.Drawing.Point(1274, 17)
+        Me.CloseWindow3.Name = "CloseWindow3"
+        Me.CloseWindow3.Size = New System.Drawing.Size(28, 26)
+        Me.CloseWindow3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.CloseWindow3.TabIndex = 5
+        Me.CloseWindow3.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Silver
+        Me.Panel2.Controls.Add(Me.DateTimePicker1)
+        Me.Panel2.Controls.Add(Me.PictureBox1)
+        Me.Panel2.Controls.Add(Me.Button6)
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.Button3)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.ComboBox1)
+        Me.Panel2.Controls.Add(Me.TextBox3)
+        Me.Panel2.Controls.Add(Me.TextBox2)
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Controls.Add(Me.Label12)
+        Me.Panel2.Controls.Add(Me.Label11)
+        Me.Panel2.Controls.Add(Me.Label10)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.Panel3)
+        Me.Panel2.Controls.Add(Me.BackWindow1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(0, 58)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1315, 633)
+        Me.Panel2.TabIndex = 2
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(97, 307)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(249, 22)
+        Me.DateTimePicker1.TabIndex = 8
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(377, 94)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(240, 332)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(235, 449)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(120, 33)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Add Image "
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(235, 509)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(120, 33)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(88, 509)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(120, 33)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Edit Info"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(88, 449)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 33)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(97, 382)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(249, 24)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(97, 235)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(249, 22)
+        Me.TextBox3.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(97, 163)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(249, 22)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(97, 93)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(249, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(100, 284)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(139, 20)
+        Me.Label12.TabIndex = 3
+        Me.Label12.Text = "Date Released:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(658, 545)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(199, 20)
+        Me.Label11.TabIndex = 3
+        Me.Label11.Text = "Total Books Released:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(386, 66)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(112, 20)
+        Me.Label10.TabIndex = 3
+        Me.Label10.Text = "Book Cover:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(100, 359)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(117, 20)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Book Status:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(100, 212)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Genre:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(100, 140)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Author:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(100, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Title:"
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.ListBox1)
+        Me.Panel3.Controls.Add(Me.Label1)
+        Me.Panel3.Location = New System.Drawing.Point(642, 21)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(660, 521)
+        Me.Panel3.TabIndex = 2
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(3, 50)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(654, 468)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(179, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Book Archives"
+        '
+        'BackWindow1
+        '
+        Me.BackWindow1.Image = Global.LoginNRegisterPage.My.Resources.Resources.BackWindow
+        Me.BackWindow1.Location = New System.Drawing.Point(4, 4)
+        Me.BackWindow1.Name = "BackWindow1"
+        Me.BackWindow1.Size = New System.Drawing.Size(66, 62)
+        Me.BackWindow1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.BackWindow1.TabIndex = 1
+        Me.BackWindow1.TabStop = False
+        '
+        'FormManageBooks
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1315, 691)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FormManageBooks"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.MinimizedWindow3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MaximizeWindow3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CloseWindow3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.BackWindow1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents MinimizedWindow3 As PictureBox
+    Friend WithEvents MaximizeWindow3 As PictureBox
+    Friend WithEvents CloseWindow3 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents BackWindow1 As PictureBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+End Class
---
+++ LoginNRegisterPage/FormManageBooks.vb
@@ -0,0 +1,196 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class FormManageBooks
+    ' Import the CreateRoundRectRgn function from the Windows API
+    <DllImport("Gdi32.dll", EntryPoint:="CreateRoundRectRgn")>
+    Private Shared Function CreateRoundRectRgn(
+        ByVal nLeftRect As Integer,
+        ByVal nTopRect As Integer,
+        ByVal nRightRect As Integer,
+        ByVal nBottomRect As Integer,
+        ByVal nWidthEllipse As Integer,
+        ByVal nHeightEllipse As Integer) As IntPtr
+    End Function
+
+    ' Variables for dragging the form
+    Private drag As Boolean
+    Private mouseX As Integer
+    Private mouseY As Integer
+
+    ' Counter for total books released
+    Private totalBooksReleased As Integer = 0
+
+    Private Sub FormManageBooks_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Remove the default title bar
+        ' Me.FormBorderStyle = FormBorderStyle.None
+
+        ' Set a clean, rounded corner radius
+        Dim radius As Integer = 20  ' Use a smaller value for crisper edges
+        Me.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Me.Width, Me.Height, radius, radius))
+
+        ' Optional: Set background color to highlight edges better
+        Me.BackColor = Color.White
+
+        ' Add click event handlers
+        AddHandler CloseWindow3.Click, AddressOf CloseWindow1_Click
+        AddHandler MinimizedWindow3.Click, AddressOf MinimizedWindow1_Click
+
+        ' Add hover effect event handlers
+        AddHandler CloseWindow3.MouseEnter, AddressOf CloseWindow1_MouseEnter
+        AddHandler CloseWindow3.MouseLeave, AddressOf CloseWindow1_MouseLeave
+        AddHandler MinimizedWindow3.MouseEnter, AddressOf MinimizedWindow1_MouseEnter
+        AddHandler MinimizedWindow3.MouseLeave, AddressOf MinimizedWindow1_MouseLeave
+        AddHandler MaximizeWindow3.MouseEnter, AddressOf MaximizeWindow1_MouseEnter
+        AddHandler MaximizeWindow3.MouseLeave, AddressOf MaximizeWindow1_MouseLeave
+
+        ' Add dragging events to the Panel
+        AddHandler Panel1.MouseDown, AddressOf Panel_MouseDown
+        AddHandler Panel1.MouseMove, AddressOf Panel_MouseMove
+        AddHandler Panel1.MouseUp, AddressOf Panel_MouseUp
+
+        AddHandler Panel2.MouseDown, AddressOf Panel_MouseDown
+        AddHandler Panel2.MouseMove, AddressOf Panel_MouseMove
+        AddHandler Panel2.MouseUp, AddressOf Panel_MouseUp
+
+        Me.ActiveControl = Nothing
+
+        ' Initialize Book Status combobox
+        ComboBox1.Items.AddRange(New String() {"Available", "Returned", "Borrowed"})
+    End Sub
+
+    Private Sub CloseWindow1_Click(sender As Object, e As EventArgs)
+        Application.Exit()
+    End Sub
+
+    ' Event handlers for CloseWindow1 hover effects
+    Private Sub CloseWindow1_MouseEnter(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Hand  ' Change cursor to hand on hover
+    End Sub
+
+    Private Sub CloseWindow1_MouseLeave(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Default  ' Revert cursor to default
+    End Sub
+
+    ' Event handler for minimize button click
+    Private Sub MinimizedWindow1_Click(sender As Object, e As EventArgs)
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    ' Event handlers for MinimizedWindow1 hover effects
+    Private Sub MinimizedWindow1_MouseEnter(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Hand  ' Change cursor to hand on hover
+    End Sub
+
+    Private Sub MinimizedWindow1_MouseLeave(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Default  ' Revert cursor to default
+    End Sub
+
+    Private Sub MaximizeWindow1_MouseEnter(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Hand  ' Change cursor to hand on hover
+    End Sub
+
+    Private Sub MaximizeWindow1_MouseLeave(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Default  ' Revert cursor to default
+    End Sub
+
+    ' Mouse Down event to start dragging
+    Private Sub Panel_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        drag = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    ' Mouse Move event to drag the form
+    Private Sub Panel_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If drag Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    ' Mouse Up event to stop dragging
+    Private Sub Panel_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub MaximizeWindow1_Click(sender As Object, e As EventArgs) Handles MaximizeWindow3.Click
+        If Me.WindowState = FormWindowState.Normal Then
+            ' Maximize the form
+            Me.WindowState = FormWindowState.Maximized
+            ' Manually set the form size to fill the screen
+            Me.Bounds = Screen.PrimaryScreen.Bounds
+            ' Recalculate region for rounded corners after maximizing
+            Dim radius As Integer = 20  ' Adjust for rounded corners
+            Me.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Me.Width, Me.Height, radius, radius))
+        Else
+            ' Restore the form to its normal size
+            Me.WindowState = FormWindowState.Normal
+            ' Recalculate region for rounded corners when restored
+            Dim radius As Integer = 20
+            Me.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Me.Width, Me.Height, radius, radius))
+        End If
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ' Implement the logic for adding the book details here
+        Dim title As String = TextBox1.Text
+            Dim author As String = TextBox2.Text
+            Dim genre As String = TextBox3.Text
+            Dim dateReleased As Date = DateTimePicker1.Value
+            Dim bookStatus As String = ComboBox1.SelectedItem.ToString()
+
+            ' Add the book details to the ListBox
+            ListBox1.Items.Add($"Title: {title}, Author: {author}, Genre: {genre}, Date Released: {dateReleased.ToShortDateString()}, Status: {bookStatus}")
+
+            ' Increment the total books released and update the label
+            totalBooksReleased += 1
+            Label11.Text = $"Total Books Released: {totalBooksReleased}"
+
+            MessageBox.Show("Saved Successfully")
+            TextBox1.Clear()
+            TextBox2.Clear()
+            TextBox3.Clear()
+            DateTimePicker1.Value = DateTime.Now
+            ComboBox1.SelectedIndex = -1
+        PictureBox1.Image = Nothing
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If ListBox1.SelectedIndex <> -1 Then
+            ' Populate the text boxes with the selected item data
+            Dim selectedItem As String = ListBox1.SelectedItem.ToString()
+            Dim details As String() = selectedItem.Split(", ")
+
+            TextBox1.Text = details(0).Replace("Title: ", "").Trim()
+            TextBox2.Text = details(1).Replace("Author: ", "").Trim()
+            TextBox3.Text = details(2).Replace("Genre: ", "").Trim()
+            DateTimePicker1.Value = Date.Parse(details(3).Replace("Date Released: ", "").Trim())
+            ComboBox1.SelectedItem = details(4).Replace("Status: ", "").Trim()
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ' Remove the selected item from the ListBox
+        If ListBox1.SelectedIndex <> -1 Then
+            ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+            totalBooksReleased -= 1
+            Label11.Text = $"Total Books Released: {totalBooksReleased}"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ' Open file dialog to select an image
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Display the selected image in the PictureBox
+            PictureBox1.Image = Image.FromFile(openFileDialog.FileName)
+        End If
+    End Sub
+
+End Class
