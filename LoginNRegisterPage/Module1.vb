1b5509c5f95565703c2445418f327762ea0e38d0
Jashley Denzel <denzelbolito24@gmail.com>
2024-11-28 22:25:33 +0800

    Revert "Add project files."
    This reverts commit 6a2f66e6ba6b343838455aa0c3709d2a532dd779.

--- LoginNRegisterPage/Module1.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports MySql.Data.MySqlClient
-Module Module1
-    Public con As New MySqlConnection("server=127.0.0.1;username=root;password=;database=lbdb")
-    Public cmd As New MySqlCommand
-
-
-
-
-End Module
