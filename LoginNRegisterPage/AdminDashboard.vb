3e33f591929f6871f57c3781065988b758447d99
Jashley Denzel <denzelbolito24@gmail.com>
2024-11-28 22:26:00 +0800

    Reapply "Add project files."
    This reverts commit 1b5509c5f95565703c2445418f327762ea0e38d0.

---
+++ LoginNRegisterPage/AdminDashboard.vb
@@ -0,0 +1,142 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports System.Reflection.Emit
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+Public Class AdminDashboard
+    ' Import the CreateRoundRectRgn function from the Windows API
+    <DllImport("Gdi32.dll", EntryPoint:="CreateRoundRectRgn")>
+    Private Shared Function CreateRoundRectRgn(
+            ByVal nLeftRect As Integer,
+            ByVal nTopRect As Integer,
+            ByVal nRightRect As Integer,
+            ByVal nBottomRect As Integer,
+            ByVal nWidthEllipse As Integer,
+            ByVal nHeightEllipse As Integer) As IntPtr
+    End Function
+
+    ' Variables for dragging the form
+    Private drag As Boolean
+    Private mouseX As Integer
+    Private mouseY As Integer
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Remove the default title bar
+        ' Me.FormBorderStyle = FormBorderStyle.None
+
+        ' Set a clean, rounded corner radius
+        Dim radius As Integer = 20  ' Use a smaller value for crisper edges
+        Me.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Me.Width, Me.Height, radius, radius))
+
+        ' Optional: Set background color to highlight edges better
+        Me.BackColor = Color.White
+
+        ' Add click event handlers
+        AddHandler CloseWindow2.Click, AddressOf CloseWindow2_Click
+        AddHandler MinimizedWindow2.Click, AddressOf MinimizedWindow2_Click
+
+        ' Add hover effect event handlers
+        AddHandler CloseWindow2.MouseEnter, AddressOf CloseWindow2_MouseEnter
+        AddHandler CloseWindow2.MouseLeave, AddressOf CloseWindow2_MouseLeave
+        AddHandler MinimizedWindow2.MouseEnter, AddressOf MinimizedWindow2_MouseEnter
+        AddHandler MinimizedWindow2.MouseLeave, AddressOf MinimizedWindow2_MouseLeave
+        AddHandler MaximizeWindow2.MouseEnter, AddressOf MaximizeWindow2_MouseEnter
+        AddHandler MaximizeWindow2.MouseLeave, AddressOf MaximizeWindow2_MouseLeave
+
+        ' Add dragging events to the Panel
+        AddHandler Panel1.MouseDown, AddressOf Panel_MouseDown
+        AddHandler Panel1.MouseMove, AddressOf Panel_MouseMove
+        AddHandler Panel1.MouseUp, AddressOf Panel_MouseUp
+
+        AddHandler Panel2.MouseDown, AddressOf Panel_MouseDown
+        AddHandler Panel2.MouseMove, AddressOf Panel_MouseMove
+        AddHandler Panel2.MouseUp, AddressOf Panel_MouseUp
+    End Sub
+
+    Private Sub CloseWindow2_Click(sender As Object, e As EventArgs)
+        Application.Exit()
+    End Sub
+
+    ' Event handlers for CloseWindow1 hover effects
+    Private Sub CloseWindow2_MouseEnter(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Hand  ' Change cursor to hand on hover
+    End Sub
+
+    Private Sub CloseWindow2_MouseLeave(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Default  ' Revert cursor to default
+    End Sub
+
+    ' Event handler for minimize button click
+    Private Sub MinimizedWindow2_Click(sender As Object, e As EventArgs)
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    ' Event handlers for MinimizedWindow1 hover effects
+    Private Sub MinimizedWindow2_MouseEnter(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Hand  ' Change cursor to hand on hover
+    End Sub
+
+    Private Sub MinimizedWindow2_MouseLeave(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Default  ' Revert cursor to default
+    End Sub
+
+    Private Sub MaximizeWindow2_MouseEnter(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Hand  ' Change cursor to hand on hover
+    End Sub
+
+    Private Sub MaximizeWindow2_MouseLeave(sender As Object, e As EventArgs)
+        CType(sender, PictureBox).Cursor = Cursors.Default  ' Revert cursor to default
+    End Sub
+
+    ' Mouse Down event to start dragging
+    Private Sub Panel_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        drag = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    ' Mouse Move event to drag the form
+    Private Sub Panel_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If drag Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    ' Mouse Up event to stop dragging
+    Private Sub Panel_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub MaximizeWindow2_Click(sender As Object, e As EventArgs) Handles MaximizeWindow2.Click
+        If Me.WindowState = FormWindowState.Normal Then
+            ' Maximize the form
+            Me.WindowState = FormWindowState.Maximized
+            ' Manually set the form size to fill the screen
+            Me.Bounds = Screen.PrimaryScreen.Bounds
+            ' Recalculate region for rounded corners after maximizing
+            Dim radius As Integer = 20  ' Adjust for rounded corners
+            Me.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Me.Width, Me.Height, radius, radius))
+        Else
+            ' Restore the form to its normal size
+            Me.WindowState = FormWindowState.Normal
+            ' Recalculate region for rounded corners when restored
+            Dim radius As Integer = 20
+            Me.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Me.Width, Me.Height, radius, radius))
+        End If
+    End Sub
+
+    Private Sub btnManageUsers1_Click(sender As Object, e As EventArgs) Handles btnManageUsers1.Click
+        FormManageUsers.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnLogout1_Click(sender As Object, e As EventArgs) Handles btnLogout1.Click
+        LoginPage.Show()
+        Me.Close()
+    End Sub
+    Private Sub btnManageBooks1_Click(sender As Object, e As EventArgs) Handles btnManageBooks1.Click
+        FormManageBooks.Show()
+        Me.Hide()
+    End Sub
+End Class
---
+++ LoginNRegisterPage/Module1.vb
@@ -0,0 +1,9 @@
+﻿Imports MySql.Data.MySqlClient
+Module Module1
+    Public con As New MySqlConnection("server=127.0.0.1;username=root;password=;database=lbdb")
+    Public cmd As New MySqlCommand
+
+
+
+
+End Module
